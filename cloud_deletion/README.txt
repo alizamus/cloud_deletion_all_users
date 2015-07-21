6a9a1be16ed5724ef7b171cdcd33c43dbd01e343
Ali Zamani <areza@juniper.net>
2015-07-21 15:13:58 -0400

    using co[y module instead of downloading the codes using git

---
+++ cloud_deletion/README.txt
@@ -0,0 +1,4 @@
+# cloud_deletion
+This repository is to delete all of the users except demo and admin.
+The codes will be cloned run using and ansible in another automation vm.
+These code should be run in the config node.
---
+++ cloud_deletion/delete-tenants.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from keystoneclient.v2_0 import client as kclient
+from novaclient import client as nclient
+import sys
+import os
+import getopt
+
+def main(argv):
+	try:
+	        opts, args = getopt.getopt(argv, "ha:", ["help", "admin_pass="])
+	except getopt.GetoptError:                                
+	        sys.exit(2)
+	
+	for opt, arg in opts:       
+	        if opt in ("-h", "--help"):        
+	                sys.exit()        
+	        elif opt in ("-a", "--admin_pass"):
+	                admin_pass = arg
+	
+	keystone = kclient.Client(username='admin', password=admin_pass , tenant_name='admin', auth_url="http://127.0.0.1:5000/v2.0")
+	predef_tenants = ['admin','demo','service','invisible_to_admin']
+	tenants = []
+	tenants_id = []
+	tenants_list = keystone.tenants.list()
+	if tenants_list:
+		for i in range(len(tenants_list)):
+			if tenants_list[i].name not in predef_tenants:
+				tenants_list[i].delete()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])		
